645704b8ffaea2070824cb5b2add2d40b06812cd
Michael Snoyman <michael@snoyman.com>
2016-09-18 21:00:37 +0300

    Watch the 01-index file instead

--- run.sh
+++ run.sh
@@ -13,4 +13,4 @@ git clone git@github.com:commercialhaskell/all-cabal-metadata --depth=1
 git config --global user.email michael+all-cabal-tool@snoyman.com
 git config --global user.name all-cabal-tool
 
-/usr/local/bin/hackage-watcher http://hackage.fpcomplete.com/00-index.tar.gz /usr/local/bin/run-inner.sh
+/usr/local/bin/hackage-watcher http://hackage.fpcomplete.com/01-index.tar.gz /usr/local/bin/run-inner.sh
